5d75abde6c640cbbdcdc4813dbbc780817404ec8
Shuja <shujakhter105@gmail.com>
2026-04-18 07:52:16 +0530

    initial setup

---
+++ Server/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Server running");
+});
+
+app.listen(5000, () => console.log(`Server started listening on port: ${PORT}`));
+
