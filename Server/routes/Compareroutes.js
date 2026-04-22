b532174659115216684cabd9120286ab43b6133e
Shuja <shujakhter105@gmail.com>
2026-04-23 01:00:30 +0530

    fix: remove incorrectly cased files

--- Server/routes/Compareroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const protect = require("../middlewares/authMiddleware");
-const { comparePapers } = require("../controllers/compareController");
-
-router.use(protect);
-router.get("/", comparePapers);
-
-module.exports = router;
